0ace38e79ca7b76ae93d5dd5d7701b9611042db5
DonivanA <64321719+DonivanA@users.noreply.github.com>
2020-04-25 14:05:25 -0500

    Update insertion_sort.cpp

--- Source_Code/insertion_sort.cpp
+++ Source_Code/insertion_sort.cpp
@@ -1,7 +1,7 @@
+#include<stdlib.h>
+#include<stdio.h>
 #include "mpi.h"
-#include <iostream>
-#include <cstdlib>
-using namespace std;
+
 /*CSCI 475 
 insertion sort
 Donivan Anderson
@@ -15,37 +15,46 @@ Brent Clapp*/
 5. repeat to 1 until each process is sorted.
 */
 int PQsort(int nelements, int *elements, int pivot, MPI_Comm comm){
-	int myrank, grp_size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);		//find rank
+/*	int myrank, grp_size;
+	int *buf;
+	/*MPI_Comm_rank(MPI_COMM_WORLD, &myrank);		//find rank
 	MPI_Comm_size(MPI_COMM_WORLD, &grp_size);	//find group size
 	pivot = rand() % (nelements-1);			//select a pivot within the range of size
 	int x = nelements % grp_size;			//x is the number of elements % number of processors
 	int send_count = nelements / grp_size;		//send_count is the number of elements / number of processors
+	int *displs=(int* )malloc(grp_size*sizeof(int));
 	
 	if(x!=0 && myrank <= x){
-		int data[send_count+1]	//number of elements to send is counting for remainders.
+		int data[send_count+1];	//number of elements to send is counting for remainders.
 	}
 	else{
 		int data[send_count];	//Data to be sent
 	}
 	
 	buf = (int* )malloc(send_count*sizeof(int));
-	MPI_scatterv(elements, send_count,MPI_INT,buf,send_count,MPI_INT,0,MPI_COMM_WORLD);
+	MPI_Scatterv(*elements, send_count,displs,MPI_INT,buf,send_count,MPI_INT,0,MPI_COMM_WORLD);*/
+}
+void display(int *a[],int size){
+	int i;
+	printf("/n The array is");
+	for(i=0;i<size;i++)
+		printf("/n %d", *a[i]);	
 }
 
 int main(int argc, char *argv[]){
+	int myrank,i, size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);		//find rank
 	MPI_Init(&argc,&argv);
-	int myrank;
-	int array;
 	size = 20;
+	int *array[20];
 	if (myrank==0){
-		for(int i=0;i<20;i++)
-			array[i] = rand;
+		for(i=0;i<size;i++)
+			*array[i] = rand()%size;
 	}		
 		
-	//size = 20, array *array, pivot = 0(changes in pivot), MPI_Comm comm 
-	PQsort(20, array, 0, MPI_COMM_WORLD);
+	//size = size, array *array, pivot = 0(changes in pivot), MPI_Comm comm 
+	PQsort(size, *array, 0, MPI_COMM_WORLD);
+	display(array,size);
 	MPI_Finalize();
 }
 
