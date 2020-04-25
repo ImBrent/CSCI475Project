8ff3c273f41535efe7af44d8860840a7e7d1d0c9
DonivanA <64321719+DonivanA@users.noreply.github.com>
2020-04-25 13:05:38 -0500

    Update insertion_sort.cpp

--- Source_Code/insertion_sort.cpp
+++ Source_Code/insertion_sort.cpp
@@ -1,5 +1,6 @@
+#include "mpi.h"
 #include <iostream>
-#include <stdlib>
+#include <cstdlib>
 using namespace std;
 /*CSCI 475 
 insertion sort
@@ -13,30 +14,39 @@ Brent Clapp*/
 4. assign elements to processes (but we need to keep processes separate from last pivot)
 5. repeat to 1 until each process is sorted.
 */
-PQsort(int nelements, int *elements, int pivot, MPI_Comm comm){
-	MPI_Init(&argc,&argv);
+int PQsort(int nelements, int *elements, int pivot, MPI_Comm comm){
+	int myrank, grp_size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);		//find rank
 	MPI_Comm_size(MPI_COMM_WORLD, &grp_size);	//find group size
-	pivot = rand % (nelements-1);			//select a pivot within the range of size
-	x = nelements % grp_size;			//x is the number of elements % number of processors
-	send_count = nelements / grp_size;		//y is the number of elements / number of processors
+	pivot = rand() % (nelements-1);			//select a pivot within the range of size
+	int x = nelements % grp_size;			//x is the number of elements % number of processors
+	int send_count = nelements / grp_size;		//send_count is the number of elements / number of processors
 	
 	if(x!=0 && myrank <= x){
 		int data[send_count+1]	//number of elements to send is counting for remainders.
 	}
-	else
+	else{
 		int data[send_count];	//Data to be sent
+	}
 	
 	buf = (int* )malloc(send_count*sizeof(int));
-	MPI_scatterV(elements, send_count,MPI_Int,buf,send_count,MPI_Int,0,MPI_COMM_WORLD)
+	MPI_scatterv(elements, send_count,MPI_INT,buf,send_count,MPI_INT,0,MPI_COMM_WORLD);
 }
 
 int main(int argc, char *argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);		//find rank
+	MPI_Init(&argc,&argv);
 	int myrank;
-	if (myrank==0)
-		int array{7,13,18,2,17,1,14,20,6,10,15,9,3,16,19,4,11,12,5,8};//This array is the same one in the book
-	
+	int array;
+	size = 20;
+	if (myrank==0){
+		for(int i=0;i<20;i++)
+			array[i] = rand;
+	}		
+		
 	//size = 20, array *array, pivot = 0(changes in pivot), MPI_Comm comm 
 	PQsort(20, array, 0, MPI_COMM_WORLD);
+	MPI_Finalize();
 }
+
+
